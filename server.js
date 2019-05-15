49b5cbe97e2707aa72f9d4071fd7d14fee50575e
austin-rey <austinrey95@gmail.com>
2019-05-15 15:26:20 -0600

    Defined routes to api endpoints

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const app = express();
 
 //Connect Database
 connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 //Looks for enviorment variable port
 const PORT = process.env.PORT || 5000;
