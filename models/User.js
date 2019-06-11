f8fa6fbe1f6188a8522d9450ceb83fa32e530164
austin-rey <austinrey95@gmail.com>
2019-06-10 19:10:22 -0600

    Added the steps for user registration (- Implimenting JST)

--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
     type: String
   },
   date: {
-    type: date,
+    type: Date,
     default: Date.now
   }
 });
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
 
 // @route   POST api/user
 // @desc    Register user
@@ -17,12 +21,55 @@ router.post(
       'Please enter a password eith 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({
+          errors: [
+            {
+              msg: 'User already exists'
+            }
+          ]
+        });
+      }
+
+      // Get users gravatar - "s r d are options"
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      // Create user instance
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save User to database
+      await user.save();
+
+      // Return JSON Webtoken
+      res.send('User registered');
+      
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
