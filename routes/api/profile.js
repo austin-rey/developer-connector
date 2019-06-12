dd4d2eb9f516082715c09b65664cc725ff230560
austin-rey <austinrey95@gmail.com>
2019-06-12 16:55:13 -0600

    Added routes for getting all user profiles and profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -108,4 +108,40 @@ router.post(
   }
 );
 
+// @route   POST api/profile
+// @desc    Get all profiles
+// @access  Public
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/profile/user/:user_id
+// @desc    Get profile by User ID
+// @access  Public
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if ((err.kind = 'ObjectID')) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
